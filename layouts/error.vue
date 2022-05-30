2a0b188219e79076f15b47947c4a0a59a6f38747
brianmhlanga <brianmhlanga9@gmail.com>
2022-05-30 16:19:02 +0200

    cleaning

--- layouts/error.vue
+++ layouts/error.vue
@@ -5,31 +5,7 @@
 </template>
 
 <script>
-export default {
-  layout: "empty",
-  props: {
-    error: {
-      type: Object,
-      default: null
-    }
-  },
-  components: {
-    Banner: () => import("@/components/shared/lp-banner/Banner404")
-  },
-  data() {
-    return {
-      pageNotFound: "404 Not Found",
-      otherError: "An error occurred"
-    };
-  },
-  head() {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError;
-    return {
-      title
-    };
-  }
-};
+
 </script>
 
 <style scoped>
--- pages/coming-soon.vue
+++ pages/coming-soon.vue
@@ -5,24 +5,7 @@
 </template>
 
 <script>
-export default {
-  layout: "coming-soon",
-  props: {
-    error: {
-      type: Object,
-      default: null
-    }
-  },
-  components: {
-    Banner: () => import("@/components/shared/lp-banner/BannerComingSoon")
-  },
-  data() {
-    return {
-      pageNotFound: "404 Not Found",
-      otherError: "An error occurred"
-    };
-  }
-};
+
 </script>
 
 <style scoped>
