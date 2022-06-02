df5b5590db9fd44a0cdf8260cd59952558cc7646
brianmhlanga <brianmhlanga9@gmail.com>
2022-06-02 10:08:20 +0200

    Update Registration Card

--- components/registrationcard.vue
+++ components/registrationcard.vue
@@ -31,13 +31,32 @@
                     ></v-text-field>
                   </v-col>
                    <v-col cols="12" md="6" class="py-0">
-                    <v-text-field
-                      label="DOB"
-                      outlined
-                      type="text"
-                      v-model="email"
-                      placeholder="Surname"
-                    ></v-text-field>
+                    <v-menu
+      ref="menu"
+      v-model="menu"
+      :close-on-content-click="false"
+      transition="scale-transition"
+      offset-y
+      min-width="auto"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-text-field
+          v-model="date"
+          label="Birthday date"
+          prepend-icon="mdi-calendar"
+          readonly
+          v-bind="attrs"
+          v-on="on"
+        ></v-text-field>
+      </template>
+      <v-date-picker
+        v-model="date"
+        :active-picker.sync="activePicker"
+        :max="(new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10)"
+        min="1950-01-01"
+        @change="save"
+      ></v-date-picker>
+    </v-menu>
                   </v-col>
                    <v-col cols="12" md="6" class="py-0">
                     <v-text-field
@@ -45,7 +64,7 @@
                       outlined
                       type="text"
                       v-model="email"
-                      placeholder="Surname"
+                      placeholder="ID Number"
                     ></v-text-field>
                   </v-col>
                    <v-col cols="12" md="6" class="py-0">
