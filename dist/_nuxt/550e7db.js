11a1fa858ac39324cc2a544945664210b65ce614
BrighterTech <bmadire4@gmail.com>
2022-06-21 09:09:41 +0200

    push dist to live

---
+++ dist/_nuxt/550e7db.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[57],{587:function(n,t,e){"use strict";e.r(t);var c={name:"topics",data:function(){return{}},components:{}},o=e(43),component=Object(o.a)(c,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
